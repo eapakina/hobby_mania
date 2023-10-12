36ed0c7093584558ad9611629ab7fd7a586f1cc7
E.Apakina <e.apakina@gmail.com>
2023-10-12 13:35:09 +0300

    routes and school page

---
+++ client/src/components/ui/Feedbacks.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FeedbackForm from "./FeedbackForm";
+import FedbackList from "./FedbackList";
+
+export default function Feedbacks(): JSX.Element {
+    
+  return (
+
+    <>
+    <FeedbackForm />
+    <FedbackList />
+  </>
+  );
+}
