b6d9ba2ce692bc1a35d5288f45747b5ecb66af8d
E.Apakina <e.apakina@gmail.com>
2023-10-12 09:09:43 +0300

    initial release

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
+const postsRouter = require('./routes/postsRouter');
+const userRouter = require('./routes/userRouter');
+
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3002;
+
+app.use(cors({ credentials: true, origin: true }));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    name: 'sid',
+    secret: process.env.SESSION_SECRET ?? 'test',
+    resave: true,
+    store: new FileStore(),
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 12,
+      httpOnly: true,
+    },
+  }),
+);
+
+app.use('/api/posts', postsRouter);
+app.use('/api/user', userRouter);
+
+app.listen(PORT, () => console.log(`Started on port ${PORT}`));
