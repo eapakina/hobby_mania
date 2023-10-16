3f609d52bc5762449a6cece218c32360929b447a
darya <atomicleda@mail.ru>
2023-10-16 12:11:01 +0300

    feedbacks

--- server/routes/postsRouter.js
+++ server/routes/postsRouter.js
@@ -1,21 +1,19 @@
-const express = require('express');
-const {
-  Class, Day, Time, Category, School, Blog,
-} = require('../db/models');
+const express = require("express");
+const { Class, Day, Time, Category, School, Blog } = require("../db/models");
 
 const router = express.Router();
 
-router.get('/all', async (req, res) => {
+router.get("/all", async (req, res) => {
   const classes = await Class.findAll();
   res.json(classes);
 });
 
-router.get('/random', async (req, res) => {
-  console.log('----------- get ------------');
+router.get("/random", async (req, res) => {
+  console.log("----------- get ------------");
   // Находим уникальные schoolId
   const uniqueSchoolIds = await Class.findAll({
-    attributes: ['schoolId', 'createdAt'],
-    group: ['schoolId', 'createdAt'],
+    attributes: ["schoolId", "createdAt"],
+    group: ["schoolId", "createdAt"],
     // order: [['createdAt', 'DESC']],
     limit: 10,
     // include: [{ model: School }],
@@ -26,14 +24,14 @@ router.get('/random', async (req, res) => {
     where: {
       schoolId: uniqueSchoolIds.map((entry) => entry.schoolId),
     },
-    order: [['createdAt', 'DESC']],
+    order: [["createdAt", "DESC"]],
     include: [{ model: School }],
   });
 
   res.json(classes);
 });
 
-router.get('/:id/all', async (req, res) => {
+router.get("/:id/all", async (req, res) => {
   const { id } = req.params;
   const classes = await Class.findAll({
     include: [
@@ -55,12 +53,12 @@ router.get('/:id/all', async (req, res) => {
   res.json(classes);
 });
 
-router.get('/all/categorys', async (req, res) => {
+router.get("/all/categorys", async (req, res) => {
   const categorys = await Category.findAll();
   res.json(categorys);
 });
 
-router.post('/:id/add', async (req, res) => {
+router.post("/:id/add", async (req, res) => {
   const {
     className,
     desription,
@@ -98,7 +96,7 @@ router.post('/:id/add', async (req, res) => {
   res.json(createdClass);
 });
 
-router.delete('/:id/delete', async (req, res) => {
+router.delete("/:id/delete", async (req, res) => {
   try {
     await Class.destroy({ where: { id: req.params.id } });
     res.sendStatus(200);
@@ -108,7 +106,7 @@ router.delete('/:id/delete', async (req, res) => {
   }
 });
 
-router.patch('/:id/edit', async (req, res) => {
+router.patch("/:id/edit", async (req, res) => {
   const {
     className,
     desription,
@@ -148,10 +146,12 @@ router.patch('/:id/edit', async (req, res) => {
 });
 
 router
-  .route('/school/:id/')
+  .route("/school/:id/")
   .get(async (req, res) => {
-    console.log('----------- get ------------');
-    const blogEntrys = await Blog.findAll({ where: { schoolId: req.params.id } });
+    console.log("----------- get ------------");
+    const blogEntrys = await Blog.findAll({
+      where: { schoolId: req.params.id },
+    });
     res.json(blogEntrys);
   })
   .post(async (req, res) => {
@@ -159,7 +159,7 @@ router
     res.json(newBook);
   });
 
-router.route('/:id').delete(async (req, res) => {
+router.route("/:id").delete(async (req, res) => {
   try {
     await Blog.destroy({ where: { id: req.params.id } });
     res.sendStatus(200);
@@ -168,25 +168,5 @@ router.route('/:id').delete(async (req, res) => {
     res.sendStatus(500);
   }
 });
-
-//   .patch(async (req, res) => {
-//     const bookId = req.params.id;
-//     const { authtor, name, status } = req.body;
-//     console.log('мы тут', req.body);
-//     try {
-//       const [updatedRowCount] = await blog.update(
-//         { authtor, name, status },
-//         { where: { id: bookId } },
-//       );
-//       if (updatedRowCount === 1) {
-//         const bookEdit = await blog.findByPk(bookId);
-//         console.log('мы тут', bookEdit);
-
-//         // Обновление прошло успешно
-//         return res.json(bookEdit);
-//       }
-//       // Нет записи для обновления или произошла другая ошибка
-//       return res.sendStatus(403); // Например, можно отправить статус 404, если запись не найдена
-
 
 module.exports = router;
